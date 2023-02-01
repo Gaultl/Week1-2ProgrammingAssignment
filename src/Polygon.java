2c83ad1e4f60be10c08e05975d47aef70ea4c507
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-02-01 09:54:26 -0800

    Created constructors and accessors in the Polygon class

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Polygon {
+public class Polygon {
+    //instance variables
+    private int numSides;
+    private double sideLength;
+    private String shapeType;
+
+    //constructors
+    public Polygon() {
+        numSides = 0;
+        sideLength = 0;
+        shapeType = "";
+    }
+
+    public Polygon(int amtSides, double howLong, String whatShape) {
+        numSides = 0;
+        sideLength = 0;
+
+        if(amtSides >= 0) {
+            numSides = amtSides;
+        }
+
+        if(howLong >= 0){
+            sideLength = howLong;
+        }
+
+        shapeType = whatShape;
+    }
+
+    //accessors
+    public int getNumSides() {
+        return numSides;
+    }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    public String getShapeType() {
+        return shapeType;
+    }
 }
