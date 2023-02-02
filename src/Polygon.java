9b6018288b26e14718af62f332b9a37dcd1e1c1d
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-02-02 09:55:55 -0800

    Created mutators and a toString method

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 public class Polygon {
     //instance variables
     private int numSides;
@@ -5,37 +6,101 @@ public class Polygon {
     private String shapeType;
 
     //constructors
+
+    /**
+     * Creates a default polygon with three sides with a length of 1.0 units
+     */
     public Polygon() {
-        numSides = 0;
-        sideLength = 0;
-        shapeType = "";
+        numSides = 3;
+        sideLength = 1.0;
+        shapeType = "triangle";
     }
 
-    public Polygon(int amtSides, double howLong, String whatShape) {
-        numSides = 0;
-        sideLength = 0;
+    /**
+     * Creates a Polygon object with a number of sides, a side length, and a shape type.
+     *
+     * @param amtSides Number of sides
+     * @param length Side length
+     * @param whatShape shape type
+     */
+    public Polygon(int amtSides, double length, String whatShape) {
+        numSides = 3;
+        sideLength = 1.0;
 
-        if(amtSides >= 0) {
+        if(amtSides > 0) {
             numSides = amtSides;
         }
 
-        if(howLong >= 0){
-            sideLength = howLong;
+        if(length > 0){
+            sideLength = length;
         }
 
         shapeType = whatShape;
     }
 
     //accessors
+
+    /**
+     *
+     * @return The number of sides of the polygon
+     */
     public int getNumSides() {
         return numSides;
     }
 
+    /**
+     *
+     * @return The side length of a polygon
+     */
     public double getSideLength() {
         return sideLength;
     }
 
+    /**
+     *
+     * @return The name/type of the shape
+     */
     public String getShapeType() {
         return shapeType;
+    }
+
+    //mutators
+
+    /**
+     * Allows the user to change the number of sides of the Polygon object, provided the desired amtSides is >= 3
+     * @param amtSides The desired number of sides
+     */
+    public void setNumSides(int amtSides){
+        if(amtSides >= 3){
+            numSides = amtSides;
+        }
+    }
+
+    /**
+     * Allows the user to change the side length provided the desired length is >0
+     * @param length the desired side length
+     */
+    public void setSideLength(double length){
+        if(length > 0){
+            sideLength = length;
+        }
+    }
+
+    /**
+     * Allows the user to change the shape type of the Polygon object
+     * @param type The desired shape type
+     */
+    public void setShapeType(String type) {
+        shapeType = type;
+    }
+
+    //other methods
+
+
+
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("#.###");
+        return "Your shape is a " + shapeType + "\nIt has a side length of " + df.format(sideLength) +
+                "\nIt has " + numSides + " sides.";
     }
 }
--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -1,5 +1,11 @@
+import java.awt.*;
+
 public class PolygonTester {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Polygon tri = new Polygon();
+        Polygon penta = new Polygon(5, 12.9774, "Pentagon");
+
+        System.out.println(tri);
+        System.out.println(penta);
     }
 }
